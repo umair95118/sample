37d9953f29ef1161692c07e5a2dd058410861f9e
umair95118 <52730908+umair95118@users.noreply.github.com>
2020-11-03 17:47:16 -0800

    Add files via upload

---
+++ server/SampleHardware.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace myMicroservice
+{
+    public class SampleHardware
+    {
+        public bool Start  { get; set; }
+
+        public int Stop { get; set; }
+
+        public int MachineId {get;set; }
+
+        
+        public string RandomDigit { get; set; }
+    }
+}
